1f72fcca8445d8dc154fe4bda907315e677183c9
Arsh Kumar <54078399+Arsh-ak7@users.noreply.github.com>
2020-10-20 15:13:04 +0530

    Add files via upload

---
+++ Todo/build/precache-manifest.39cda0fbca579b66cc9fbd12618c8053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a91da9a230cd631b878a46cd805ea1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aaf317c55be07444f3a",
+    "url": "/static/css/main.d1fb729b.chunk.css"
+  },
+  {
+    "revision": "0fc042de3bcd68429def",
+    "url": "/static/js/2.6efbc0bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6efbc0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aaf317c55be07444f3a",
+    "url": "/static/js/main.cd9585cf.chunk.js"
+  },
+  {
+    "revision": "454958551820eab5eadf",
+    "url": "/static/js/runtime-main.26981f68.js"
+  }
+]);
