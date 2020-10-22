32dca581617cf4b36dc80f690060595708e76d72
Arsh-ak7 <arshkr07@gmail.com>
2020-10-22 17:32:38 +0530

    Update

---
+++ WeatherApp/src/assests/WeatherIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function WeatherIcon({weather}) {
+    return(
+        <div className="main-weather-icon">
+            <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} className="weather-icon" alt="Not Found" />
+        </div>
+    )
+}
