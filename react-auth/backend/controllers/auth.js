985d600f2df54859b4bf90927dd8524031f39c24
Arsh-ak7 <arshkr07@gmail.com>
2021-06-10 17:52:33 +0530

    Did forget password email send

--- react-auth/backend/controllers/auth.js
+++ react-auth/backend/controllers/auth.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const sendMail = require("../utils/sendEmail");
+const ErrorResponse = require("../utils/errorResponse");
 
 exports.register = async (req, res, next) => {
 	const { username, email, password } = req.body;
@@ -45,8 +47,43 @@ exports.login = async (req, res, next) => {
 	}
 };
 
-exports.forgotpassword = (req, res, next) => {
-	res.send("forgot password Route");
+exports.forgotpassword = async (req, res, next) => {
+	const { email } = req.body;
+
+	try {
+		const user = await User.findOne({ email });
+		if (!user)
+			res.status(404).json({
+				success: false,
+				error: "User not found",
+			});
+		const resetToken = user.getResetToken();
+		await user.save();
+		const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`;
+		const message = `
+			<h1> You have requested a password reset<h1>
+			<p>Go to the link provided</p>
+			<a href = ${resetUrl} clicktracking =off>${resetUrl}</a>
+		`;
+		try {
+			await sendMail({
+				to: user.email,
+				subject: "Password Reset",
+				text: message,
+			});
+			res.status(200).json({
+				success: true,
+				data: "Email sent",
+			});
+		} catch (error) {
+			user.resetPasswordToken = undefined;
+			user.resetPasswordExpire = undefined;
+			await user.save();
+			return next(new ErrorResponse("Email not sent"));
+		}
+	} catch (err) {
+		return next(new ErrorResponse("Forget not sent"));
+	}
 };
 
 exports.resetpassword = (req, res, next) => {
