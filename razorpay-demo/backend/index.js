f2f398b66d22e9e7674a77720dba3694d650d97c
Arsh-ak7 <arshkr07@gmail.com>
2021-06-28 10:23:27 +0530

    implemented a web-hook

--- razorpay-demo/backend/index.js
+++ razorpay-demo/backend/index.js
@@ -3,10 +3,12 @@ const path = require("path");
 const cors = require("cors");
 const Razorpay = require("razorpay");
 const shortid = require("shortid");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 var razorpay = new Razorpay({
 	key_id: "rzp_test_nHaVbpE0xKVSil",
@@ -38,6 +40,33 @@ app.post("/razorpay", async (req, res) => {
 	} catch (err) {
 		console.log(err);
 	}
+});
+
+app.post("/verification", (req, res) => {
+	// do a validation
+	const secret = "123456789";
+
+	console.log(req.body);
+
+	const crypto = require("crypto");
+
+	const shasum = crypto.createHmac("sha256", secret);
+	shasum.update(JSON.stringify(req.body));
+	const digest = shasum.digest("hex");
+
+	console.log(digest, req.headers["x-razorpay-signature"]);
+
+	if (digest === req.headers["x-razorpay-signature"]) {
+		console.log("request is legit");
+		// process it
+		require("fs").writeFileSync(
+			"payment1.json",
+			JSON.stringify(req.body, null, 4)
+		);
+	} else {
+		// pass it
+	}
+	res.json({ status: "ok" });
 });
 
 app.listen(1337, () => {
